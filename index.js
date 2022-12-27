5f1b30bfa0f245f6bd9f507b68376498bbc602b4
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2022-12-27 11:07:09 +0100

    I'm actually getting better at JavaScript

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 let close = document.getElementById("closebtn")
-close.addEventListener("click" , function(){this.parentElement.style.display='none'})
-console.log(2)
+close.addEventListener("click" , closeParent)
+
+let close2 = document.getElementById("closebtn2")
+close2.addEventListener("click" , closeParent)
+
+let button = document.getElementsByClassName("button")[0]
+button.addEventListener("click" , closeParent )
+
+
+function closeParent(){this.parentElement.style.display='none'}
+
+
+let toggle = document.getElementById("toggle")
+function showFilters(){
+    (document.getElementsByClassName("whenyouclick")[0]).style.display = "block"
+}
+toggle.addEventListener("click", showFilters)
+
+ 
 
 
 
+let addToCart = document.querySelectorAll(".addtocart")
+for(i=0; i< addToCart.length; i++){
+    (addToCart[i]).addEventListener("click", function(){alert("Item has been added to cart")})
+}
+
+
+function addedToCart(){
+    document.getElementById("cart-alert").style.display = "block";
+}
 
 
 
@@ -20,10 +46,11 @@ function increaseCartNumbering() {
     }
 }
 increaseCartNumbering()
-increaseCartNumbering()
 
 
-function addedToCart(){
 
+let product = document.querySelectorAll(".product")
+console.log(product)
+for(i=0; i< product.length; i++){
+    console.log(product[i])
 }
-
